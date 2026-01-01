fa05d50b592997fd908eb7c606af9c2e824b615c
Bradley <15671567+bradleyhodges@users.noreply.github.com>
2026-01-01 16:30:46 +0800

    refactor: upgrade dependencies to latest compatible versions

--- tooling/scripts/check-version.js
+++ tooling/scripts/check-version.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { satisfies } = require("semver");
-const { engines } = require("../package");
+const { engines } = require("../../package");
 
 const version = engines.node;
 if (!satisfies(process.version, version)) {
